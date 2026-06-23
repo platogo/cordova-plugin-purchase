b262fef5a769b7bcf77569051941504a6b7eb87f
Rinald Shabani <rinald@platogo.com>
2026-06-23 13:35:02 +0200

    Do not md5 encrypt accountId as it is used to persist restore required data

--- www/store.js
+++ www/store.js
@@ -5761,7 +5761,7 @@ var CdvPurchase;
             function extendAdditionalData(ad) {
                 const additionalData = ensureObject(ad === null || ad === void 0 ? void 0 : ad.googlePlay);
                 if (!additionalData.accountId && (ad === null || ad === void 0 ? void 0 : ad.applicationUsername)) {
-                    additionalData.accountId = CdvPurchase.Utils.md5(ad.applicationUsername);
+                    additionalData.accountId = ad.applicationUsername;
                 }
                 return additionalData;
             }
